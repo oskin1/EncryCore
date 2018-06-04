6765b38fec6f3a9d8ef51c2a38ebb94546eee3cb
kapinuss <kapinuss@rambler.ru>
2018-06-04 12:07:57 +0300

    begin

--- src/main/scala/encry/EncryApp.scala
+++ src/main/scala/encry/EncryApp.scala
@@ -43,12 +43,12 @@ object EncryApp extends App with ScorexLogging {
   lazy val encrySettings: EncryAppSettings = EncryAppSettings.read(args.headOption)
 
   lazy val settings: ScorexSettings = encrySettings.scorexSettings
+  require(settings.network.agentName.length <= 50)
 
   implicit val system: ActorSystem = ActorSystem(settings.network.agentName)
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-  require(settings.network.agentName.length <= 50)
   lazy val bindAddress: InetSocketAddress = settings.restApi.bindAddress
 
   lazy val timeProvider: NetworkTimeProvider = new NetworkTimeProvider(settings.ntp)
